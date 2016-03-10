fb6472be9d6fbbdab9ea67e7d94441d0e6a1d7c4
anildias <anildiasdavis@gmail.com>
2016-03-10 11:29:15 +0530

    Version bump to 0.1.1

---
+++ lib/locator/unit.rb
@@ -0,0 +1,14 @@
+module Locator	
+	module Unit
+		DEFAULT_UNIT = "km"
+		AVAILABLE_UNITS = ["km", "mile", "radians", "degrees"]
+
+		def self.default_unit
+			DEFAULT_UNIT
+		end
+
+		def self.available_units
+			AVAILABLE_UNITS
+		end
+	end
+end
